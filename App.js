6c90912ce7a52579ebf42ac85ad4bbbb8ffb674d
eliomor <45327873+eliomor@users.noreply.github.com>
2021-04-05 21:20:15 +0300

    BSPM2021T29-48
    Update the App component

--- App.js
+++ App.js
@@ -1,9 +1,51 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import ReduxThunk from 'redux-thunk';
+import { Ionicons } from '@expo/vector-icons';
+import * as Font from 'expo-font';
+import AppLoading from 'expo-app-loading';
 
 import AppNavigator from '../ScruMaster/navigation/AppNavigator';
+import projectsReducer from './store/reducer/projects';
+import sprintsReducer from './store/reducer/sprints';
+import tasksReducer from './store/reducer/tasks';
+import usersReducer from './store/reducer/users';
+import authReducer from './store/reducer/auth';
+
+const rootReducer = combineReducers({
+  projects: projectsReducer,
+  sprints: sprintsReducer,
+  tasks: tasksReducer,
+  users: usersReducer,
+  auth: authReducer
+});
+
+
+const store = createStore(rootReducer, applyMiddleware(ReduxThunk));
+
+const fetchFonts = () => {
+  return Font.loadAsync({
+    ...Ionicons.font,
+  });
+};
+
+
 export default function App() {
+ const [fontLoaded, setFontLoaded] = useState(false);
+  if (!fontLoaded) {
+    return (
+      <AppLoading
+        startAsync={fetchFonts}
+        onFinish={() => {setFontLoaded(true); }}
+        onError={console.warn}
+      />
+    );
+  }
+
   return (
-    <AppNavigator/>
+    <Provider store={store}>
+      <AppNavigator/>
+    </Provider>
   );
 }
-
