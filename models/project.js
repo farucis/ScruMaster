36cd994b0dba2ba252f3292277f488dea6d16d62
faruch ismailov <58651410+farucis@users.noreply.github.com>
2021-04-05 21:39:23 +0300

    BSPM2021T29-62
    create model for project

---
+++ models/project.js
@@ -0,0 +1,9 @@
+class Project {
+    constructor(projectId,OwnerId,title) {
+        this.projectId = projectId;
+        this.OwnerId=OwnerId;
+        this.title = title;
+    }
+}
+
+export default Project;
