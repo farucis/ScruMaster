a1b011b5683310fca43673d9561c9730ee71ec0b
idan <58389697+idanbn@users.noreply.github.com>
2021-04-05 21:31:23 +0300

    BSPM2021T29-50
    create test folder

---
+++ test/.expo/app.test.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import App from '../App';
+import * as auth from '../store/action/auth';
+
+import configureStore from 'redux-mock-store';
+
+test('The App renders correctly', () => {
+  const tree = renderer.create(<App />).toJSON();
+  expect(tree).toMatchSnapshot();
+});
+
+
+const middlewares = []
+const mockStore = configureStore(middlewares)
+
+const login = () => ({ type: 'LOGIN' })
+
+test('should dispatch LOGIN  action', () => {
+
+  const initialState = {}
+  const store = mockStore(initialState)
+
+  store.dispatch(login())
+
+  const actions = store.getActions()
+  const expectedPayload = { type: 'LOGIN' }
+  expect(actions).toEqual([expectedPayload])
+})
+
+const signup = () => ({ type: 'SIGNUP' })
+
+test('should dispatch SIGNUP action', () => {
+
+  const initialState = {}
+  const store = mockStore(initialState)
+
+  store.dispatch(signup())
+
+  const actions = store.getActions()
+  const expectedPayload = { type: 'SIGNUP' }
+  expect(actions).toEqual([expectedPayload])
+})
