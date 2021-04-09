77ca5d2ec974c77093bba06899d8b800aecf3169
yehudavallach <yv12345@gmail.com>
2021-04-09 11:02:39 +0300

    BSPM2021T29-16
    create UI for project item

---
+++ components/ProjectItem.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import { View, Text, StyleSheet, Button } from 'react-native';
+
+import Colors from '../constants/Colors';
+import Card from '../components/UI/Card';
+
+const ProjectItem = props => {
+  return (
+    
+    <Card style={styles.container}>
+      <View style={styles.details}>
+        <Text style={styles.title}>{props.title}</Text>
+      </View>
+      <View style={styles.actions}>
+        <Button
+          color={Colors.primary}
+          title="View Details"
+          onPress={props.onViewDetail}
+        />
+        <Button
+          color={Colors.primary}
+          title="Delete"
+          onPress={props.onDelete}
+        />
+      </View>
+    </Card>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    height: 140,
+    margin: 20
+   },
+
+  details: {
+    alignItems: 'center',
+    height: '15%',
+    padding: 10
+  },
+  title: {
+    fontSize: 30,
+    marginVertical: 4
+  },
+  actions: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    height: '75%',
+    paddingHorizontal: 20
+  }
+});
+
+export default ProjectItem;
