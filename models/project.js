d8ab0d39857ccf8f66b0a7548c02615b2acadfce
idan <58389697+idanbn@users.noreply.github.com>
2021-04-11 13:21:21 +0300

    Revert "Merge branch 'main' of https://github.com/idanbn/ScruMaster into main"
    This reverts commit 4bd6f79ec097d757b0e9a7688218361ab74e56ca, reversing
    changes made to 48777c562c0b5fc3a2d3e21d74f88aa03e6c86ca.

--- models/project.js
+++
@@ -1,9 +0,0 @@
-class Project {
-    constructor(projectId,OwnerId,title) {
-        this.projectId = projectId;
-        this.OwnerId=OwnerId;
-        this.title = title;
-    }
-}
-
-export default Project;
