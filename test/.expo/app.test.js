230f5a28db9ee45665905f034b585208f120681f
yehudavallach <yv12345@gmail.com>
2021-04-18 14:49:20 +0300

    SPM2021T29-103
    create tests for create, delete, fetch project from the DB

--- test/.expo/app.test.js
+++ test/.expo/app.test.js
@@ -45,3 +45,66 @@ test('should dispatch SIGNUP action', () => {
   const expectedPayload = { type: 'SIGNUP' }
   expect(actions).toEqual([expectedPayload])
 })
+
+//test fetch Projects
+const fetchProjects = () => ({ type: 'SET_PROJETCS' })
+
+test('should dispatch SET_PROJETCS action', () => {
+
+  const initialState = {}
+  const store = mockStore(initialState)
+
+  store.dispatch(fetchProjects())
+
+  const actions = store.getActions()
+  const expectedPayload = { type: 'SET_PROJETCS' }
+  expect(actions).toEqual([expectedPayload])
+})
+
+//test fetch My Projects
+
+const fetchMyProjects = () => ({ type: 'SET_MY_PROJETCS' })
+
+test('should dispatch SET_MY_PROJETCS action', () => {
+
+  const initialState = {}
+  const store = mockStore(initialState)
+
+  store.dispatch(fetchMyProjects())
+
+  const actions = store.getActions()
+  const expectedPayload = { type: 'SET_MY_PROJETCS' }
+  expect(actions).toEqual([expectedPayload])
+})
+
+//test delete Project
+
+const deleteProject = () => ({ type: 'DELETE_PROJECT' })
+
+test('should dispatch DELETE_PROJECT action', () => {
+
+  const initialState = {}
+  const store = mockStore(initialState)
+
+  store.dispatch(deleteProject())
+
+  const actions = store.getActions()
+  const expectedPayload = { type: 'DELETE_PROJECT' }
+  expect(actions).toEqual([expectedPayload])
+})
+
+//test create Project
+
+const createProject = () => ({ type: 'CREATE_PROJECT' })
+
+test('should dispatch CREATE_PROJECT action', () => {
+
+  const initialState = {}
+  const store = mockStore(initialState)
+
+  store.dispatch(createProject())
+
+  const actions = store.getActions()
+  const expectedPayload = { type: 'CREATE_PROJECT' }
+  expect(actions).toEqual([expectedPayload])
+})
