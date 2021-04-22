c4a81657faab79472471719a1a05c7bbc7a6f184
eliomor <45327873+eliomor@users.noreply.github.com>
2021-04-22 19:06:32 +0300

    BSPM2021T29-122
    Add users to project

---
+++ screens/add/AddUsersScreen.js
@@ -0,0 +1,50 @@
+import  React,  { useEffect } from 'react';
+import { FlatList, StyleSheet } from 'react-native';
+import { useSelector, useDispatch } from 'react-redux';
+import { LinearGradient } from 'expo-linear-gradient';
+
+import AddUserItem from '../../components/AddUserItem';
+import * as usersAction from '../../store/action/users';
+
+const AddUsersScreen = (props) => {
+   const dispatch = useDispatch()
+   const projectId = props.navigation.getParam('projectId');
+   const ownerId = useSelector(state => state.projects.availableProjects.find(proj => proj.projectId === projectId).ownerId);
+   users = useSelector(state => state.users.availableUsers);
+
+   useEffect(() => {
+    dispatch(usersAction.fetchUsers());
+  }, [dispatch]);
+
+
+   return (
+     <LinearGradient colors={['#6F7AED','#ffe3ff']} style={styles.gradient}> 
+        <FlatList
+          data={users}
+          keyExtractor={item => item.userId}
+          renderItem={itemData => (
+            <AddUserItem
+              name={itemData.item.name}
+              onAddUser={() => {
+                dispatch(usersAction.addUserToProject(itemData.item.userId, projectId));
+                dispatch(usersAction.fetchUsers());
+              }}
+            />       
+          )}
+        />
+   </LinearGradient>
+  );
+};
+
+const styles = StyleSheet.create({
+  gradient: {
+    width: '100%',
+    height: '100%',
+    justifyContent: 'center',
+    alignItems: 'center'
+  }
+ }
+);
+
+export default AddUsersScreen;
+
