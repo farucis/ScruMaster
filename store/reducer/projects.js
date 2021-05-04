7e5a6816f318847a892c33e17f6eff075483da82
idan <58389697+idanbn@users.noreply.github.com>
2021-05-04 17:09:38 +0300

    BSPM2021T29-159
    create project for test

---
+++ store/reducer/projects.js
@@ -0,0 +1,40 @@
+import Project from '../../models/project';
+
+import { DELETE_PROJECT, CREATE_PROJECT, SET_PROJETCS } from '../action/projects';
+
+
+const initialState = {
+    availableProjects: [],
+    userProjects: []
+};
+
+export default (state = initialState, action) => {
+    switch (action.type) {
+        case SET_PROJETCS: 
+            return {
+                availableProjects: action.projects,
+                userProjects: action.userProjects
+            };
+        case CREATE_PROJECT:
+             const newProject = new Project(
+                 action.projectData.projectId,
+                 action.projectData.ownerId,
+                 action.projectData.title
+                 );
+                return {
+                    ...state,
+                    availableProjects: state.availableProjects.concat(newProject),
+                    userProjects: state.userProjects.concat(newProject)
+                };
+
+        case DELETE_PROJECT:
+            return {
+                ...state,
+                userProjects: state.userProjects.filter(project => project.projectId !== action.pid),
+                availableProjects: state.availableProjects
+                .filter(project => project.projectId !== action.pid),
+
+            };
+    }
+    return state;
+};
