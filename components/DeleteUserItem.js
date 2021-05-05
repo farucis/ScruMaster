c7e00c9b2924175cb9c51e6310ae531878d2e6df
eliomor <45327873+eliomor@users.noreply.github.com>
2021-05-05 21:09:08 +0300

    BSPM2021T29-104
    design for delete user

---
+++ components/DeleteUserItem.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import { View, Text, StyleSheet, Button } from 'react-native';
+
+import Colors from '../constants/Colors';
+import Card from './UI/Card';
+
+const DeleteUserItem = props => {
+  return (
+    
+    <Card style={styles.container}>
+      <View style={styles.details}>
+        <Text style={styles.title}>{props.name}</Text>
+      </View>
+      <View style={styles.topActions}>
+       <View style={styles.actions}>
+        <View style={styles.actions}>
+        <Button
+          color={Colors.primary}
+          title="Delete User"
+          onPress={props.onDeleteUser}
+        />
+        </View>
+       </View>
+      </View>
+    </Card>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    height: 140,
+    width: 300,
+    margin: 20
+   },
+  details: {
+    alignItems: 'center',
+    height: '25%',
+    padding: 10
+  },
+  title: {
+    fontSize: 30,
+    marginVertical: 4
+  },
+  topActions: {
+    marginTop: 30,
+    justifyContent: 'center',
+  },
+  actions: {
+    justifyContent: 'center',
+    flexDirection: 'row',
+
+  },
+});
+export default DeleteUserItem;
