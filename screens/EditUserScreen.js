7cad43ebafcd79d7bdfd602970c88a234d44bdfc
yehudavallach <yv12345@gmail.com>
2021-05-19 19:27:15 +0300

    BSPM2021T29-67
    create the edit user screen

---
+++ screens/EditUserScreen.js
@@ -0,0 +1,109 @@
+import React, { useState, useEffect, useCallback } from 'react';
+import { View, ScrollView, Text, TextInput, StyleSheet, Platform,KeyboardAvoidingView, TouchableOpacity } from 'react-native';
+import { useSelector, useDispatch } from 'react-redux';
+
+import * as usersAction from '../store/action/users';
+import Colors from '../constants/Colors';
+
+const EditUsersScreen = (props) => {
+  const dispatch = useDispatch()
+  const userId = props.navigation.getParam('userId');
+  const userExists = useSelector(state =>
+    state.users.availableUsers.find(user => user.userId === userId)
+  );
+
+  useEffect(() => {
+   dispatch(usersAction.fetchUsers());
+ }, [dispatch]);
+ 
+  const [name,setName] = useState(userExists ? userExists.name : '');
+  const [phone,setPhone] = useState(userExists ? userExists.phone : '');
+  const [age,setAge] = useState(userExists ? userExists.age : '');
+
+  const submitHandler = useCallback(() => {
+    dispatch(usersAction.updateUser(userId ,name, phone, age));
+    dispatch(usersAction.fetchUsers());
+    props.navigation.goBack();
+  }, [dispatch,userId ,name, phone, age]);
+
+  useEffect(() => {
+    props.navigation.setParams({ submit: submitHandler });
+  }, [submitHandler]);
+
+  return (
+  <KeyboardAvoidingView style={{ flex: 1 }} behavior="height'">  
+    <ScrollView>
+      <View style={styles.form}>      
+        <View style={styles.formControl}>
+          <Text style={styles.label}>Name</Text>
+          <TextInput
+            style={styles.input}
+            value={name}
+            onChangeText={text => setName(text)}
+            autoCapitalize='sentences'
+          />
+        </View>
+        <View style={styles.formControl}>
+          <Text style={styles.label}>Phone</Text>
+          <TextInput
+            style={styles.input}
+            value={phone}
+            onChangeText={text => setPhone(text)}
+            autoCapitalize='sentences'
+          />
+        </View>
+        <View style={styles.formControl}>
+          <Text style={styles.label}>Age</Text>
+          <TextInput
+            style={styles.input}
+            value={age}
+            onChangeText={text => setAge(text)}
+            autoCapitalize='sentences'
+          />
+        </View>
+      </View>
+    </ScrollView>
+  </KeyboardAvoidingView>
+  );
+};
+
+EditUsersScreen.navigationOptions = navData => {
+  const submitFn = navData.navigation.getParam('submit');
+  return {
+    headerTitle: 'Edit User',
+    headerRight: (
+      <TouchableOpacity style={styles.headerButton} onPress={submitFn}>
+      <Text style={styles.headerButtonText}>Save </Text>
+      </TouchableOpacity>
+    )
+  };
+};
+
+const styles = StyleSheet.create({
+  form: {
+    margin: 20
+  },
+  formControl: {
+    width: '100%'
+  },
+  label: {
+    fontFamily: 'open-sans-bold',
+    marginVertical: 8
+  },
+  input: {
+    paddingHorizontal: 2,
+    paddingVertical: 5,
+    borderBottomColor: '#ccc',
+    borderBottomWidth: 1
+  },
+  headerButton: {
+    marginHorizontal: 20
+  },
+  headerButtonText: {
+    fontSize: 16,
+    color: Colors.primary
+  }
+});
+
+export default EditUsersScreen;
+
